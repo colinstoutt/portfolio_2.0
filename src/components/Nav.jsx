53fe7266479b425b18af999a5726b562749cb7eb
cps97 <colinstoutt@gmail.com>
2023-02-26 18:35:29 -0800

    add nav options to top

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,28 @@
 import "../scss/Nav.scss";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="nav">
+      <a className="nav__link" href="/">
+        <span className="nav__link-name">Colin Stoutt</span>
+        <br />
+        <span>Web Developer</span>
+      </a>
+      <div>
+        <a className="nav__link" href="https://github.com/colinstoutt">
+          <GitHubIcon sx={{ fontSize: "2rem" }} />
+        </a>
+        <a
+          className="nav__link"
+          href="https://www.linkedin.com/in/colin-stoutt/"
+        >
+          <LinkedInIcon sx={{ fontSize: "2rem", marginLeft: "0.5rem" }} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
